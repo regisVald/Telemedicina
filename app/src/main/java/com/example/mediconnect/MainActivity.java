6799ac81fac376bb1391fc17c6fea50b49d23aa5
valstre <regisvaldivieso16@gmail.com>
2024-03-14 21:49:18 -0600

    Feat: Creacion base de datos y tabla CT_Rol

--- app/src/main/java/com/example/mediconnect/MainActivity.java
+++ app/src/main/java/com/example/mediconnect/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.mediconnect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.mediconnect.db.dbHelper;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnCrear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCrear = findViewById(R.id.btnCrear);
+
+        btnCrear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dbHelper dbHelper = new dbHelper(MainActivity.this);
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                if(db != null){
+                    Toast.makeText(MainActivity.this, "Base de datos creada", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this, "Error al crear la base de datos", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/mediconnect/db/dbHelper.java
@@ -0,0 +1,36 @@
+package com.example.mediconnect.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class dbHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NOMBRE = "MediConnect.db";
+    public static final String TABLE_ROL = "CT_ROL";
+
+
+
+    public dbHelper(@Nullable Context context) {
+        super(context, DATABASE_NOMBRE, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + TABLE_ROL + "(" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "nombre TEXT NOT NULL," +
+                "estodo INTEGER NOT NULL," +
+                "fechaCreacion DATE NOT NULL," +
+                "fechaActualizado DATE)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE " + TABLE_ROL);
+        onCreate(db);
+    }
+}
